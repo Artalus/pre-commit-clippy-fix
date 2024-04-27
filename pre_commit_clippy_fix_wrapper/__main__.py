6ac9fadfa3385e74f6ca9052b38a2ef77d58d3b7
Artalus <artalus-mail@yandex.ru>
2024-04-27 16:43:45 +0400

    working script

---
+++ pre_commit_clippy_fix_wrapper/__main__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import subprocess
+import sys
+
+
+def main() -> int:
+    _, *script_args = sys.argv
+
+    cmd = ['cargo', 'clippy', '--fix', '--allow-staged', *script_args]
+    process = subprocess.Popen(cmd, stderr=subprocess.PIPE)
+    _, stderr = process.communicate()
+    rc = process.wait()
+    if rc != 0:
+        print(f"{' '.join(cmd)} exited with {rc}; STDERR was:\n", file=sys.stderr)
+        print(stderr.decode(), file=sys.stderr)
+    return rc
+
+
+if __name__ == '__main__':
+    sys.exit(main())
